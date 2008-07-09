3808e13ac1f0204884702859dc35598ab37d07f4
Simon Schoeters <hamfilter@gmail.com>
2008-07-09 00:02:45 +0000

    Initial import

---
+++ classes/ZipCarton.h
@@ -0,0 +1,62 @@
+/*
+ Copyright (c) 2008 Simon Schoeters
+ 
+ Permission is hereby granted, free of charge, to any person
+ obtaining a copy of this software and associated documentation
+ files (the "Software"), to deal in the Software without
+ restriction, including without limitation the rights to use,
+ copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the
+ Software is furnished to do so, subject to the following
+ conditions:
+ 
+ The above copyright notice and this permission notice shall be
+ included in all copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ OTHER DEALINGS IN THE SOFTWARE.
+ 
+ Created by Simon Schoeters on 2008.07.03.
+*/
+
+#import <AddressBook/AddressBook.h>
+#import "ZipCarton.h"
+
+@interface FindPostalCode : NSObject
+{
+}
+
+// Returns the property this action is for.
+- (NSString *)actionProperty;
+
+// Returns the title for this action. The current person is passed in as person.
+// If the actionProperty is a multiValue type, identifier will contain the identifier
+// of the item the user rolled over. If the actionProperty is not a multiValue type
+// identifier will be nil.
+- (NSString *)titleForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+
+// This method is called when the user selects your action. As above, this method
+// is passed information about the data item rolled over.
+- (void)performActionForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+
+// Optional. Your action will always be enabled in the absence of this method. As
+// above, this method is passed information about the data item rolled over.
+- (BOOL)shouldEnableActionForPerson:(ABPerson *)person identifier:(NSString *)identifier;
+
+// Translates the given city to a postal code and returns the postal code as a string. Includes the country (as ISO code!) in the search if given to limit the results.
+- (NSString *)postalCodeForCity:(NSString *)city country:(NSString *)country;
+
+// Tries to find the ISO code for the given country string, search is multi-lingual.
+- (NSString *)findISOforCountry:(NSString *)country;
+
+// Filters the returned list for this city to more relevant results (only exact matches)
+- (NSArray *)filterList:(NSArray *)unfiltered filter:(NSString *)filter;
+
+@end
+
